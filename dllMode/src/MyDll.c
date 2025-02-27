a5d7dd927b424cdff4462025f8d7d94b3fe3ca74
baizhanwuyibubai <1372749708@qq.com>
2025-02-27 22:48:19 +0800

    first commit

---
+++ dllMode/src/MyDll.c
@@ -0,0 +1,10 @@
+﻿#include "MyDll.h"
+#include <stdio.h>
+
+void my_function(void) {
+    printf("Hello from DLL!\n");
+}
+
+int add(int a, int b) {
+    return a + b;
+}
---
+++ dllMode/src/MyDll.h
@@ -0,0 +1,20 @@
+﻿#ifndef MY_DLL_H
+#define MY_DLL_H
+#ifdef _WIN32
+    #ifdef BUILD_MY_DLL
+        #define DLL_API __declspec(dllexport)
+    #else
+        #define DLL_API __declspec(dllimport)
+    #endif
+#else
+    #if defined(__GNUC__) && (__GNUC__ >= 4)
+        #define DLL_API __attribute__((visibility("default")))
+    #else
+        #define DLL_API 
+    #endif
+#endif
+
+DLL_API void my_function(void);
+DLL_API int add(int a, int b);
+
+#endif // MY_DLL_H
